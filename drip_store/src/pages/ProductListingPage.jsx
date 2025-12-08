b6715e7fe19c2715bdd57a795e22c5d6c12c3b0f
Lenizio27 <155769957+Lenizio27@users.noreply.github.com>
2025-12-08 14:49:42 -0300

    estilização rota de produtos no checkbox
    feita nova estilização no checkbox e aba para resultados de pesquisa

--- drip_store/src/pages/ProductListingPage.jsx
+++ drip_store/src/pages/ProductListingPage.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const ProductListingPage = () => {
     const arrayFiltroProdutos = [
         {
@@ -21,17 +23,16 @@ const ProductListingPage = () => {
         <>
         <section className="bg-s6">
             <div className="flex flex-col max-w-[1440px] m-auto px-4">
-                <div>
-                    <h2></h2>
-                    <div>
+                <div className="flex justify-between items-center">
+                    <h2 className="">resultados para ""</h2>
+                    <div className="w-[300px] h-[50px] flex items-center justify-center border">
                         ordenar por:
-                        <select name="" id="">
-                            <option value="">mais relevanes</option>
-                            <option value="">sasdfasdf</option>
+                        <select name="" id="" className="bg-s6">
+                            <option value="" className="bg-c2">mais relevanes</option>
                         </select>
                     </div>
                 </div>
-                <div className="flex p-4">
+                <div className="flex">
                     <aside className="flex-4 bg-s7 p-4 w-[308px]">
                         <h3>Filtrar por</h3>
                         <hr />
@@ -39,8 +40,12 @@ const ProductListingPage = () => {
                             <div>
                                 <h2 className="font-bold my-1">{item.title}</h2>
                                 {item.radios.map(a => 
-                                <p>
-                                    <input type="radio" id="radioCheck"/>{a}
+                                <p className="flex items-center">
+                                    <input 
+                                    className="appearance-none w-4 h-4 border border-gray-400 rounded-sm checked:bg-c2 checked:border-purple-600 checked:before:content-['✓'] flex items-center text-white mr-1"
+                                    type="checkbox" 
+                                    id="radioCheck"/>
+                                    {a}
                                 </p>
                                 )}
                             </div>
